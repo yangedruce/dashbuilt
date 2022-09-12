d8b7d254960a86f7a0bfdfcf8b7f3e10936941f2
Izyan Idrus <hello@yangedruce.com>
2022-09-12 21:05:30 +0800

    fix nav, fix footer, fix table, add forms, typgraphy, cards at home

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,6 @@ const setup = () => {
     return {
         isSidebarOpen: false,
         currentSidebarTab: null,
-        isSettingsPanelOpen: false,
         watchScreen() {
             if (window.innerWidth <= 1024) {
                 this.isSidebarOpen = false
